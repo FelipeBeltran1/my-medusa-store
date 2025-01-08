6e94fb96298710061f3c7eb7ba881f8637ab5132
FelipeBeltran1 <an.cadena@udla.edu.co>
2025-01-08 16:13:24 -0500

    Add event emission for brand creation in createBrandWorkflow

--- src/workflows/create-brand.ts
+++ src/workflows/create-brand.ts
@@ -6,6 +6,7 @@ import {
 } from "@medusajs/framework/workflows-sdk"
 import { BRAND_MODULE } from "../modules/brand"
 import BrandModuleService from "../modules/brand/service"
+import { emitEventStep } from "@medusajs/medusa/core-flows"
 
 export type CreateBrandStepInput = {
   name: string
@@ -39,6 +40,13 @@ export const createBrandWorkflow = createWorkflow(
   "create-brand",
   (input: CreateBrandWorkflowInput) => {
     const brand = createBrandStep(input)
+
+    emitEventStep({
+      eventName: "brand.created",
+      data: {
+        id: brand.id,
+      }
+    })
     
     return new WorkflowResponse(brand)
   }
